b27ccbfcb95538b46a0ea103523cb489e2afa38f
limbermuffin <limbermuffin@gmail.com>
2017-11-29 15:52:33 -0800

    Score display for pong

--- Assets/PongController.cs
+++ Assets/PongController.cs
@@ -1,39 +1,39 @@
 ﻿using UnityStandardAssets.CrossPlatformInput;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PongController : MonoBehaviour {
-    public int speed = 10;
     public GameObject ball;
     public GameObject player;
     public GameObject enemy;
+    public Text scoreText;
+    public int speed = 10;
+
     private Rigidbody rbEnemy;
     private Rigidbody rbPlayer;
     private Rigidbody rbBall;
     private int score;
 
-    // Use this for initialization
     void Start() {
         rbBall = ball.GetComponent<Rigidbody>();
-        rbBall.velocity = new Vector3(speed, speed, 0);
         rbPlayer = player.GetComponent<Rigidbody>();
         rbEnemy = enemy.GetComponent<Rigidbody>();
+        rbBall.velocity = new Vector3(speed, speed, 0);
         score = 0;
     }
 
-    // Update is called once per frame
     void Update() {
         if (Mathf.Abs(ball.transform.position.x) > 10) {
-            rbBall.velocity = new Vector3(speed * Mathf.Sign(ball.transform.position.x), speed, 0);
+            rbBall.velocity = new Vector3(speed * Mathf.Sign(ball.transform.position.x), Random.Range(-speed, speed), 0);
             score += (int) Mathf.Sign(ball.transform.position.x);
+            scoreText.text = score.ToString();
             ball.transform.position = Vector3.zero;
+        }
+        if (Mathf.Abs(score) >= 5) {
+            SceneManager.LoadScene("main");
         }
         rbPlayer.velocity = new Vector3(0, CrossPlatformInputManager.GetAxisRaw("Vertical") * speed, 0);
         rbEnemy.velocity = new Vector3(0, CrossPlatformInputManager.GetAxisRaw("Vertical") * speed, 0);
-        if (Mathf.Abs(score) >= 5) {
-            Application.LoadLevel("main");
-        }
     }
 }
