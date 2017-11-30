337a48cb47fa2d5d8b0fd412a3417239eee10da1
limbermuffin <limbermuffin@gmail.com>
2017-11-29 16:58:24 -0800

    Pong AI

--- Assets/PongController.cs
+++ Assets/PongController.cs
@@ -16,24 +16,38 @@ public class PongController : MonoBehaviour {
     private int score;
 
     void Start() {
+        // get rigidbodies
         rbBall = ball.GetComponent<Rigidbody>();
         rbPlayer = player.GetComponent<Rigidbody>();
         rbEnemy = enemy.GetComponent<Rigidbody>();
+
+        // set ball starting velocity
         rbBall.velocity = new Vector3(speed, speed, 0);
+
+        // zero score
         score = 0;
     }
 
     void Update() {
+        // handle scoring
         if (Mathf.Abs(ball.transform.position.x) > 10) {
             rbBall.velocity = new Vector3(speed * Mathf.Sign(ball.transform.position.x), Random.Range(-speed, speed), 0);
-            score += (int) Mathf.Sign(ball.transform.position.x);
+            score += (int)Mathf.Sign(ball.transform.position.x);
             scoreText.text = score.ToString();
             ball.transform.position = Vector3.zero;
         }
+
+        // load main scene if score magnitude greater than 5
         if (Mathf.Abs(score) >= 5) {
             SceneManager.LoadScene("main");
         }
+
+        // player paddle movement
         rbPlayer.velocity = new Vector3(0, CrossPlatformInputManager.GetAxisRaw("Vertical") * speed, 0);
-        rbEnemy.velocity = new Vector3(0, CrossPlatformInputManager.GetAxisRaw("Vertical") * speed, 0);
+
+        // enemy paddle movement
+        var pos = enemy.transform.position;
+        pos.y = Mathf.MoveTowards(pos.y, ball.transform.position.y, Time.deltaTime * speed * 0.4f);
+        enemy.transform.position = pos;
     }
 }
