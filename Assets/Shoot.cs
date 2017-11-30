44cc42faad6f489bc3cbfc622917454410bb69a0
limbermuffin <limbermuffin@gmail.com>
2017-11-29 16:10:51 -0800

    Refactored shoot + changed sounds around

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -2,26 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shoot : MonoBehaviour
-{
+public class Shoot : MonoBehaviour {
     public GameObject Ball;
-    public int Speed;
 
-    private Transform _cam;
-
-    // Use this for initialization
-    void Start()
-    {
-        _cam = GetComponentInChildren<Transform>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetButtonDown("Shoot"))
-        {
-            Instantiate(Ball, _cam.position + _cam.forward * 2, _cam.rotation);
+    void Update() {
+        if (Input.GetButtonDown("Shoot")) {
+            Instantiate(Ball, transform.position + transform.forward * 2, transform.rotation);
         }
     }
-
 }
